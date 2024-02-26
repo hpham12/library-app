b8496987492bc6110356fc3eb6d89c07dacbc735
Hieu Pham <hieuminhphamcse21@gmail.com>
2024-02-26 06:56:59 -0500

    feat: call API from Carousel component

--- library-app-frontend/src/layouts/homepage/components/Carousel/Carousel.tsx
+++ library-app-frontend/src/layouts/homepage/components/Carousel/Carousel.tsx
@@ -1,7 +1,62 @@
+import { useEffect, useState } from 'react';
 import { ReturnBook } from '../ReturnBook/ReturnBook';
 import './Carousel.css'
+import { BookModel } from '../../../../models/BookModel';
 
 export const Carousel = () => {
+	const [books, setBooks] = useState<BookModel[]>([]); 
+	const [isLoading, setIsLoading] = useState<boolean>(true);
+	const [httpError, setHttpError] = useState<string|null>(null);
+
+	useEffect(() => {
+		(async () => {
+			const baseUrl: string = "http://localhost:8080/api/books";
+			const url: string = `${baseUrl}?page=0&size=9`;
+			const response = await fetch(url);
+			if (!response.ok) {
+				setIsLoading(false);
+				const error = new Error(`Failed to fetch ${url}`);
+				setHttpError(error.message);
+				throw new Error(`Failed to fetch ${url}`);
+			}
+			const jsonResponse = await response.json();
+			const responseData = jsonResponse._embedded.books;
+			const loadedBooks: BookModel[] = [];
+			for (const key in responseData) {
+				loadedBooks.push(
+					{
+						id: responseData[key].id,
+						title: responseData[key].title,
+						author: responseData[key].author,
+						description: responseData[key].description,
+						copies: responseData[key].copies,
+						copiesAvailable: responseData[key].copiesAvailable,
+						category: responseData[key].category,
+						image: responseData[key].image
+					}
+				);
+			}
+			setBooks(loadedBooks);
+			setIsLoading(false);
+		})();
+	}, []);
+
+	if (isLoading) {
+		return (
+			<div className='container m-5'>
+				<p>Loading...</p>
+			</div>
+		)
+	}
+
+	if (httpError) {
+		return (
+			<div className='container m-5'>
+				<p>{httpError}</p>
+			</div>
+		)
+	}
+
 	return (
 		<div className='container mt-5 carousel-wrapper'>
 			<div className='homepage-carousel-title'>
@@ -14,26 +69,26 @@ export const Carousel = () => {
 				<div className='carousel-inner'>
 					<div className='carousel-item active'>
 						<div className='d-flex row justify-content-center align-items-center'>
-							<ReturnBook/>
-							<ReturnBook/>
-							<ReturnBook/>
+							{books.slice(0, 3).map(book => (
+								<ReturnBook book={book} key={book.id}/>
+							))}
 						</div>
 					</div>
 					<div className='carousel-item'>
 						<div className='d-flex row justify-content-center align-items-center'>
-							<ReturnBook/>
-							<ReturnBook/>
-							<ReturnBook/>
+							{books.slice(3, 6).map(book => (
+								<ReturnBook book={book} key={book.id}/>
+							))}
 						</div>
 					</div>
 					<div className='carousel-item'>
 						<div className='d-flex row justify-content-center align-items-center'>
-							<ReturnBook/>
-							<ReturnBook/>
-							<ReturnBook/>
+							{books.slice(6, 9).map(book => (
+								<ReturnBook book={book} key={book.id}/>
+							))}
 						</div>
 					</div>
-					</div>
+				</div>
 					<button className='carousel-control-prev' type='button' data-bs-target='#carouselExampleControls' data-bs-slide='prev'>
 						<span className='carousel-control-prev-icon' aria-hidden='true'></span>
 						<span className='visually-hidden'> Previous</span>
@@ -48,7 +103,7 @@ export const Carousel = () => {
 				{/* Mobile */}
 				<div className='d-lg-none mt-3'>
 					<div className='row d-flex justify-content-center align-items-center'>
-						<ReturnBook/>
+						<ReturnBook book={books[7]} key={books[7].id}/>
 					</div>
 				</div>
 				<div className='homepage-carousel-title mt-3'>
--- library-app-frontend/src/layouts/homepage/components/ReturnBook/ReturnBook.tsx
+++ library-app-frontend/src/layouts/homepage/components/ReturnBook/ReturnBook.tsx
@@ -1,20 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { BookModel } from '../../../../models/BookModel';
 
-export const ReturnBook = () => {
+interface ReturnBookProps {
+	book: BookModel;
+}
+
+export const ReturnBook = (props: ReturnBookProps) => {
+	const {book} = props;
+
 	return (
 		<div className='col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3'>
-								<div className='text-center'>
-									<img 
-										src={require('./../../../../Images/BooksImages/book-luv2code-1000.png')}
-										width='151'
-										height='233'
-										alt='book'
-										>
-									</img>
-									<h6 className='mt-2'>Book</h6>
-									<p>Luv2Code</p>
-									<a className='btn main-color text-white' href='/#'>Reserve</a>
-								</div>
-							</div>
+			<div className='text-center'>
+				{book.image? 
+				<img
+					src={book.image}
+					width='151'
+					height='233'
+					alt='book'
+				>
+				</img>:
+				<img 
+					src={require('./../../../../Images/BooksImages/book-luv2code-1000.png')}
+					width='151'
+					height='233'
+					alt='book'
+					>
+				</img>
+			}
+				
+				<h6 className='mt-2'>{book.title}</h6>
+				<p>{book.author}</p>
+				<a className='btn main-color text-white' href='/#'>Reserve</a>
+			</div>
+		</div>
 	);
 }
